d2ff270d42b7c32d020f85afbefdc77b65e6a717
Thomas Werner <twerner@mountainnews.com>
2014-12-12 19:23:50 +0100

    oops, I lost some changes on image_classification. style improvements

--- http_server.py
+++ http_server.py
@@ -6,25 +6,65 @@ import string,cgi,time
 from os import curdir, sep
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 #import pri
+import os
+import sys
 
 class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            #if self.path.endswith(".html"):
+            if ( self.path.endswith(".css") or self.path.endswith(".jpg") ):
+                f = open(curdir + sep + self.path) #self.path has /test.html
+                self.send_response(200)
+                self.send_header('Content-type',  'text/html' if self.path.endswith(".css") else "image/jpg" )
+                self.end_headers()
+                self.wfile.write( f.read() )
+                f.close()
 
-            self.send_response(200)
-            self.send_header('Content-type',    'text/html')
-            self.end_headers()
-            parse_domain = self.path[1:]
-            if parse_domain != "":
-                return self.html_download( parse_domain )
-            self.wfile.write("hey, today is the" + str(time.localtime()[7]))
-            self.wfile.write(" day in the year " + str(time.localtime()[0]))
+            else: 
+
+                self.send_response(200)
+                self.send_header('Content-type',    'text/html')
+                self.end_headers()
+
+                self.wfile.write( "<link href=\"styles.css\" rel=\"stylesheet\" type=\"text/css\">" )
+
+                if self.path.endswith("test"):
+                    for root, _, files in os.walk("/data/git/image-classification/testdata"):
+                        for file_name in files:
+                            file_path = os.path.join(root, file_name)
+                            response = subprocess.check_output("python image_classification.py image \""+ file_path +"\"", shell=True)
+                            m = re.search('\[(.)\].*image:\s(.*)$', response)
+                            print( response )
+                            img_url = m.group(2)
+                            is_logo = m.group(1)
+
+
+                            self.wfile.write( "<div class=\"res result"+is_logo+"\"><img src=\"testdata/" +  file_path[40:] + "\" /></div>" )
+                            sys.stdout.flush()
+
+                    return
+
+
+                else: 
+                    parse_domain = self.path[1:]
+                    if parse_domain != "":
+                        return self.html_download( parse_domain )
+                    self.wfile.write("This is your company logo download service<br /><br />try <a href=\"/test\">/test</a> to show results of image classification with SVM. (it will show testresults for all images in the subdirectory 'testdata/' and its subdirs)" )
+                    self.wfile.write("<br />try <a href=\"/www.lego.com\">/www.lego.com</a> any domain to extract the logo of that domain. Check the A.I. response to see what the SVM thinks of it: [1] = it is a logo, [0] = it is not a logo." )
             return
                 
         except IOError:
             self.send_error(404,'File Not Found: %s' % self.path)
+
+
+
+
+    def run_tests( self ):
+
+
+        return
+
 
 
     def html_download( self, url ):
